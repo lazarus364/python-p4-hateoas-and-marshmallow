b080576d77d0e3ea2f969b562551243b8b613553
lazarus kipchumba <lazaruskipchumba6@gmail.com>
2025-02-21 16:49:50 +0300

    marshmallow

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+# server/app.py
+
 #!/usr/bin/env python3
 
 from flask import Flask, request, make_response
@@ -14,6 +16,29 @@ app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
+
+ma = Marshmallow(app)
+
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
 
 api = Api(app)
 
@@ -116,7 +141,6 @@ class NewsletterByID(Resource):
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
